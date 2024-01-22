e4f8fd395276fcf3e58ec5a348c0dfb8c9ce443b
Martin Munene <munene02@gmail.com>
2024-01-22 17:40:27 +0300

    add order routes

--- application/routes.go
+++ application/routes.go
@@ -5,15 +5,25 @@ import (
 
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/munene02/go-microservice-orders-api/handler"
 )
 
 func loadRoutes() *chi.Mux{
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
+	
 	router.Get("/", func(w http.ResponseWriter, r *http.Request){
 		w.WriteHeader(http.StatusOK)
 	})
-	
+
+	router.Route("/orders", loadOrderRoutes())
+
 	return router
 }
+
+func loadOrderRoutes(router chi.Router){
+	return 
+}
+
+
 
