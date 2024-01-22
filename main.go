9fd5074693a57fc4f8bc18e70100e0a4f65973d0
Martin Munene <munene02@gmail.com>
2024-01-22 15:38:05 +0300

    add get route

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"github.com/go-chi/chi/v5gi"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main(){
+	router := chi.NewRouter()
+
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
 		Addr: ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router, //http.HandlerFunc(basicHandler),
 	}
 
 	err := server.ListenAndServe()
